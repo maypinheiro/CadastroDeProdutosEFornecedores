ceea9fcf1060eb7c62278db1d214ad6866882b39
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-10-04 17:20:07 -0300

    Projeto MVC 5

---
+++ src/DevIO.Business/Models/Fornecedores/IEnderecoRepository.cs
@@ -0,0 +1,15 @@
+﻿using DevIO.Business.Core.Data;
+using DevIO.Business.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ src/DevIO.Business/Models/Produtos/Produto.cs
@@ -0,0 +1,20 @@
+﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Models.Fornecedores;
+using System;
+
+namespace DevIO.Business.Models.Produtos
+{
+  public class Produto : Entity
+  {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* Ef Relation */
+        public Fornecedor Fornecedor { get; set; }
+  }
+}
