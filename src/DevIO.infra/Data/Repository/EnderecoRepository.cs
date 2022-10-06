d05f35b57a4106fa47fd1f2304b05a0237985546
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-10-06 12:33:06 -0300

    Repositórios especializados

---
+++ src/DevIO.infra/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.infra.Data.Context;
+using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace DevIO.infra.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await ObterPorId(fornecedorId);
+        }
+    }
+}
