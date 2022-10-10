bb6b5aa982999a316a8b4c7510068c0eb44b6de6
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-10-10 15:58:34 -0300

    Fluente validation Produtoo

---
+++ src/DevIO.Business/Models/Produtos/Validations/ProdutoValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace DevIO.Business.Models.Produtos.Validations
+{
+    class ProdutoValidation : AbstractValidator<Produto>
+    {
+        public ProdutoValidation()
+        {
+            RuleFor(x => x.Nome)
+                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                 .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(x => x.Descrição)
+                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                 .Length(2, 1000).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(x => x.Valor)
+                .GreaterThan(0)
+                .WithMessage("O campo {PropertyName} precisa ser maior que {ComparisonVaue}");
+
+        }
+    }
+}
