8bfbe95cf3c8266269652ac9d6d48cfa5f9b5479
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-10-10 15:47:41 -0300

    Fluente validation Endereco

--- src/DevIO.Business/Models/Fornecedores/Validacoes/EnderecoValidation.cs
+++ src/DevIO.Business/Models/Fornecedores/Validacoes/EnderecoValidation.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace DevIO.Business.Models.Fornecedores.Validacoes
 {
-    class EnderecoValidation
+    public class EnderecoValidation : AbstractValidator<Endereco>
     {
+        public EnderecoValidation()
+        {
+            RuleFor(x => x.Logradouro)
+                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                 .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(x => x.Bairro)
+                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                 .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(x => x.Cep)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(8).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(x => x.Cidade)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(x => x.Estado)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+            
+            RuleFor(x => x.Numero)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(1, 10).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
     }
 }
